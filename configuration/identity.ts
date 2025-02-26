db17ce96e9d6452c4a01701b7c8efa1fd0c565e4
GloryoCR <gloryocostarica@gmail.com>
2025-02-25 21:45:15 -0500

    Update AI Name
    Changed [AI Name] to Rami

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Glory`;
 export const OWNER_DESCRIPTION: string = `UNC MBA passionate about empowering international students`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Rami`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
