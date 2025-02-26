46c24d383fb9757ae6a4bd7b4b7b6e7baadd0f93
GloryoCR <gloryocostarica@gmail.com>
2025-02-25 21:50:40 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Glory`;
-export const OWNER_DESCRIPTION: string = `UNC MBA passionate about empowering international students`;
+export const OWNER_DESCRIPTION: string = `MBA from UNC passionate about empowering international students`;
 
 export const AI_NAME: string = `Rami`;
 export const AI_TONE: string = `Friendly, supportive, clear, and culturally aware`;
