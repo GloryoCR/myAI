7b1687eb765a0981c3a94f417223d66821d94092
GloryoCR <gloryocostarica@gmail.com>
2025-02-25 21:18:32 -0500

    Update Header
    Switched from [Chat Header] to Chat with Rami, your International MBA Guide!

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Chat with Rami, your International MBA Guide!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Have a Pura Vida Day!`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
