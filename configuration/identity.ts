7089c3c596805c7509fabf18d7d27a3680f89ee6
GloryoCR <gloryocostarica@gmail.com>
2025-02-25 21:41:30 -0500

    Update Owner Description
    Changed from [Owner Description] to UNC MBA passionate about empowering international students

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Glory`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `UNC MBA passionate about empowering international students`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
